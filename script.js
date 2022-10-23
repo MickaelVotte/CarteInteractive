351ec20200c97e72e91aee20ce14bf82de36e167
unknown <mickael.votte@hotmail.com>
2022-10-23 17:07:25 +0200

    selectionner des pays; foret du bassin du Congo

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+
+
+// consttruction de la carte
+
 const map = L.map('map').setView([-1.68595, 19.58426], 5);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -6,17 +11,21 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
+
+
+
+
+
+//ajout des marqueurs
+/*
 const marker1 = L.marker([-1.68595, 19.58426]).addTo(map);
 const marker2 = L.marker([-2.68550, 26.58420]).addTo(map);
 const marker3 = L.marker([-4.68895, 19.58126]).addTo(map);
 const marker4 = L.marker([-1.98095, 21.58026]).addTo(map);
 
 
-marker1.bindPopup("<img class='grandsSinges' src='/assets/img/grandssinges.jpg' alt='grands singes'><b>Les Grands Singes!<br><button type='button' class='btn-marker ms-5' data-bs-toggle='modal' data-bs-target='#grandSinges'>Plus d'info</button>").openPopup();
+marker1.bindPopup("<img class='grandsSinges' src='/assets/img/grandssinges.jpg' alt='grands singes'><b>Les Grands Singes!<br><button type='button' class='btn-marker ms-5' data-bs-toggle='modal' data-bs-target='#grandSinges'>Plus d'info</button>");
 
 
-marker2.bindPopup("<b>Hello world!</b><br>I am a Tiger.").openPopup();
-
-
-
-
+marker2.bindPopup("<b>Hello world!</b><br>I am a Tiger.")
+*/
